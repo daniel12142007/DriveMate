efdb24e43403f5290557d125dd32af3b49460729
daniel12142007 <danijelahatzanov@gmail.com>
2025-03-23 15:39:52 +0600

    Start project

---
+++ RiderServices/src/main/java/com/rider/riderservices/RiderServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rider.riderservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderServicesApplication.class, args);
+	}
+
+}
---
+++ RiderServices/src/test/java/com/rider/riderservices/RiderServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rider.riderservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
